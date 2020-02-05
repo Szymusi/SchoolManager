36713606b5508435854b073a236f1b135ba8c6e4
Szymon Danielak <danielak.szymon@gmail.com>
2020-02-06 00:19:57 +0100

    Created basic logic structure

---
+++ SchoolManager/SchoolManager.Logic/Services/GradeOperations/GradeOperationsService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using SchoolManager.Data.Models;
+
+namespace SchoolManager.Logic.Services.GradeOperations
+{
+    public class GradeOperationsService : IGradeOperationsService
+    {
+        public double CalculateWeightedAvarage(IEnumerable<Grade> grades)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ SchoolManager/SchoolManager.Logic/Services/GradeOperations/IGradeOperationsService.cs
@@ -0,0 +1,10 @@
+﻿using SchoolManager.Data.Models;
+using System.Collections.Generic;
+
+namespace SchoolManager.Logic.Services.GradeOperations
+{
+    public interface IGradeOperationsService
+    {
+        double CalculateWeightedAvarage(IEnumerable<Grade> grades);
+    }
+}
