ae494a240f9a76e694cb996d11677de024d299d6
Szymon Danielak <danielak.szymon@gmail.com>
2020-02-16 14:01:21 +0100

    Little changes before creating new branch

--- SchoolManager/SchoolManager.Logic/Services/GradeOperations/GradeOperationsService.cs
+++ SchoolManager/SchoolManager.Logic/Services/GradeOperations/GradeOperationsService.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
 using SchoolManager.Data.Models;
+using SchoolManager.Logic.Services.Users;
 
 namespace SchoolManager.Logic.Services.GradeOperations
 {
     public class GradeOperationsService : IGradeOperationsService
     {
+        private readonly IUserService _userService = new UserService();
+
         public double CalculateWeightedAvarage(IEnumerable<Grade> grades)
         {
             throw new System.NotImplementedException();
