0907463716feba8eb6367f5d3f0708af0fd0227e
Szymon Danielak <danielak.szymon@gmail.com>
2020-02-19 00:51:11 +0100

    Refactored GradeOperationsService

---
+++ SchoolManager/SchoolManager.Logic/Services/Grades/GradeService.cs
@@ -0,0 +1,33 @@
+﻿using SchoolManager.Data.Enums;
+using SchoolManager.Data.Models;
+using SchoolManager.Data.Repositories.Students;
+using System.Collections.Generic;
+
+namespace SchoolManager.Logic.Services.Grades
+{
+    public class GradeService : IGradeService
+    {
+        public Dictionary<SchoolSubjects, double> CalculateWeightedAvarages(Student student)
+        {
+            Dictionary<SchoolSubjects, double> subjectAvarageDictionary = new Dictionary<SchoolSubjects, double>();
+
+            return subjectAvarageDictionary;
+        }
+
+        public double CalculateAvarage(IEnumerable<Grade> grades)
+        {
+            double gradessum = 0;
+            int weightssum = 0;
+
+            foreach (var grade in grades)
+            {
+                gradessum += grade.Value;
+                weightssum += grade.Weight;
+            }
+
+            double avarage = gradessum / weightssum;
+
+            return avarage;
+        }
+    }
+}
---
+++ SchoolManager/SchoolManager.Logic/Services/Grades/IGradeService.cs
@@ -0,0 +1,13 @@
+﻿using SchoolManager.Data.Enums;
+using SchoolManager.Data.Models;
+using System.Collections.Generic;
+
+namespace SchoolManager.Logic.Services.Grades
+{
+    public interface IGradeService
+    {
+        Dictionary<SchoolSubjects, double> CalculateWeightedAvarages(Student student);
+
+        double CalculateAvarage(IEnumerable<Grade> grades);
+    }
+}
