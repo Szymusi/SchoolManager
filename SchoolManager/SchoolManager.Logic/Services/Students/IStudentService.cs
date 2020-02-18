2db424f1c2d55f851d5ac053c5167b9194423882
Szymon Danielak <danielak.szymon@gmail.com>
2020-02-19 00:51:40 +0100

    Added student service

---
+++ SchoolManager/SchoolManager.Logic/Services/Students/IStudentService.cs
@@ -0,0 +1,9 @@
+﻿using SchoolManager.Data.Models;
+
+namespace SchoolManager.Logic.Services.Students
+{
+    public interface IStudentService
+    {
+        Student GetStudentByUser(User user);
+    }
+}
---
+++ SchoolManager/SchoolManager.Logic/Services/Students/StudentService.cs
@@ -0,0 +1,21 @@
+﻿using SchoolManager.Data.Models;
+using SchoolManager.Data.Repositories.Students;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SchoolManager.Logic.Services.Students
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IStudentRepository _studentRepository = new MockStudentRepository();
+
+        public Student GetStudentByUser(User user)
+        {
+            IEnumerable<Student> students = _studentRepository.GetStudents();
+
+            Student student = students.SingleOrDefault(s => s.User.Id == user.Id);
+
+            return student;
+        }
+    }
+}
